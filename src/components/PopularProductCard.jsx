757915f602ca232d5813a317c597cd1517dff7a2
Aditya Rawat <adityarawat2801@gmail.com>
2023-10-07 12:45:21 +0530

    updates

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -13,7 +13,7 @@ const PopularProductCard = ({imgURL,name, price}) => {
        <p className="font-montserrat text-xl leading-normal text-slate-gray">4.5</p>
       </div>
       <h3 className="mt-2 text-2xl leadiing-normal font-semibold font-palanquin">{name}</h3>
-      <p className="mt-2 font-semibold font-montserrat text-2xl text-coral-red leading-normal ">{price}</p>
+      <p className="mt-2 font-semibold font-montserrat text-2xl text-blue-700 leading-normal ">{price}</p>
     </div>
   )
 }
